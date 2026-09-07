f460cdef890ec0bdf5b96d666d8d5097a7f84da9
Giang <giang.lai1139@gmail.com>
2026-09-07 20:21:56 +0200

    spiel neustarten

--- skripte/game_manager.gd
+++ skripte/game_manager.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var defeat_screen = $CanvasLayerDefeat
+var game_over = false
 
 func _ready() -> void:
 	defeat_screen.hide()
@@ -11,5 +12,17 @@ func _ready() -> void:
 		death_barrier.trigger_loss.connect(_trigger_loss)
 		
 func _trigger_loss() -> void:
+	game_over = true
 	defeat_screen.show()
 	get_tree().paused = true
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if not game_over:
+		return
+	# Maus freilassen
+	if Input.is_action_just_pressed("pause"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	# Neustart
+	if Input.is_action_just_pressed("restart"):
+		get_tree().paused = false
+		get_tree().reload_current_scene()
