ebffb7acf51b870c830c65a4531ce9096cb6743e
Giang <giang.lai1139@gmail.com>
2026-09-08 17:14:36 +0200

    changed logic and added win screen

--- skripte/game_manager.gd
+++ skripte/game_manager.gd
@@ -1,16 +1,39 @@
 extends Node3D
 
 @onready var defeat_screen = $CanvasLayerDefeat
+@onready var win_screen = $CanvasLayerDefeat/CanvasLayerWin
 @onready var death_barrier = get_tree().current_scene.find_child("DeathBarrier")
+@onready var coins = get_tree().current_scene.find_child("Coins")
+@onready var coin_count = coins.get_child_count()
 var game_over = false
+
+######################## Münzenlogik
+func _collected_coin() -> void:
+	coin_count -= 1
+	if coin_count == 0:
+		_trigger_win()
+
+######################## Signalkonfiguration
 
 func _ready() -> void:
 	defeat_screen.hide()
+	win_screen.hide()
 	
 	# Signal
 	if death_barrier.has_signal("trigger_loss"):
 		death_barrier.trigger_loss.connect(_trigger_loss)
 		
+	for coin in coins.get_children():
+		if coin.has_signal("collected"):
+			coin.collected.connect(_collected_coin)
+		
+######################## Game Over Bildschirm		
+
+func _trigger_win() -> void:
+	game_over = true
+	win_screen.show()
+	get_tree().paused = true
+
 func _trigger_loss() -> void:
 	game_over = true
 	defeat_screen.show()
