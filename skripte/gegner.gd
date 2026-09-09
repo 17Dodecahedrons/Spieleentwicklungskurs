bcd236a3dd2d7894830349aa253e1613ff333447
Giang <giang.lai1139@gmail.com>
2026-09-09 12:13:21 +0200

    enemy movement

---
+++ skripte/gegner.gd
@@ -0,0 +1,38 @@
+extends CharacterBody3D
+
+const SPEED = 2.0
+@onready var floor_detection = $RayCast3D
+# Variablen um Gehen und Umdrehen zu kontrollieren
+var keep_moving = true
+var still_turning = false
+
+func _physics_process(delta: float) -> void:
+	# Schwerkraft
+	var gravity = Vector3(0, -9.8, 0)
+	if not is_on_floor():
+		velocity += gravity * delta
+	
+	# Bewegung
+	if keep_moving and is_on_floor():
+		# Basiswechsel
+		velocity = -global_transform.basis.z * SPEED
+		# Überprüfe ob ein Abgrund vor uns ist
+		if not floor_detection.is_colliding():
+			keep_moving = false
+	elif is_on_floor():
+		# Nicht bewegen
+		velocity.x = 0
+		velocity.z = 0
+		# Beginne umdrehen
+		if not still_turning:
+			still_turning = true
+			# Drehe dich um
+			var target_rotation = wrapf(rotation.y + PI, -PI, PI)
+			# Dreh Animation per Tween
+			var tween = create_tween()
+			tween.tween_property(self, "rotation:y", target_rotation, 1.5)
+			await tween.finished
+			still_turning = false # Umdrehen ist fertig
+			keep_moving = true # Wir können uns wieder bewegen
+	
+	move_and_slide()
