7bbbddc9ea622f034cd4488069ae7383481149cd
Giang <giang.lai1139@gmail.com>
2026-09-11 23:58:43 +0200

    fixed a bug where a signal is always connected

--- skripte/coin_1.gd
+++ skripte/coin_1.gd
@@ -5,11 +5,13 @@ const ROTATIONS_GESCHWINDIGKEIT = 2.0
 # Mehrfachkollisionen verhindern mit Schalter
 var allow_collision = true
 
+func _ready() -> void:
+	# Signal
+	coin_body.body_entered.connect(_on_body_entered)
+
 func _process(delta: float) -> void:
 	# Rotiere
 	rotate_y(ROTATIONS_GESCHWINDIGKEIT * delta)
-	# Signal
-	coin_body.body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body: Node3D):
 	if body.is_in_group("player") and allow_collision:
