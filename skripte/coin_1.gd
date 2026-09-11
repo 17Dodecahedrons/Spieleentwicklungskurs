13c13758c247c929a25ebc71c296718f0b2c1b5a
Giang <giang.lai1139@gmail.com>
2026-09-11 16:52:47 +0200

    added tween

--- skripte/coin_1.gd
+++ skripte/coin_1.gd
@@ -1,7 +1,21 @@
 extends Node3D
 
 const ROTATIONS_GESCHWINDIGKEIT = 2.0
+@onready var coin_body = $Area3D
+# Mehrfachkollisionen verhindern mit Schalter
+var allow_collision = true
 
 func _process(delta: float) -> void:
 	# Rotiere
 	rotate_y(ROTATIONS_GESCHWINDIGKEIT * delta)
+	# Signal
+	coin_body.body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(body: Node3D):
+	if body.is_in_group("player") and allow_collision:
+		allow_collision = false
+		# Tween
+		var tween = create_tween()
+		tween.tween_property(self, "position:y", self.position.y + 1.5, 0.1)
+		await tween.finished
+		queue_free() # Lösche mich selbst
